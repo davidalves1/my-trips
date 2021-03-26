950904b58984943578802bedb209b5fdfe901018
davidalves1 <alves.david@outlook.com>
2021-03-26 00:04:24 -0300

    feat: add map component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ function App({ Component, pageProps }: AppProps) {
         {/* TODO: customize this tags for your project */}
         <title>My Awesome App - Boilerplate</title>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
         <meta name="description" content="Describe your project here" />
       </Head>
       <GlobalStyles />
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #00adb5;
+    --background: #222831;
+    --white: #eee;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -11,8 +23,27 @@ const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;
   }
 
+  html,
+  body,
+  #__next {
+    height: 100%;
+    background: var(--background);
+    color: var(--white);
+  }
+
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p,
+  a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
+    text-decoration: none;
   }
 `;
 
