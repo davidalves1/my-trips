4fbaa974553ed041a82e4a6553fcd6ea5a46ac87
davidalves1 <alves.david@outlook.com>
2021-04-06 01:51:59 -0300

    update

--- src/components/LinkWrapper/index.spec.tsx
+++ src/components/LinkWrapper/index.spec.tsx
@@ -3,9 +3,9 @@ import LinkWrapper from '.';
 
 describe('<LinkWrapper />', () => {
   it('should render link and children', () => {
-    render(<LinkWrapper href="/my-link">my link</LinkWrapper>);
+    render(<LinkWrapper href="/my-link">my awesome link</LinkWrapper>);
 
-    const children = screen.getByText(/my link/i);
+    const children = screen.getByText(/my awesome link/i);
 
     expect(children).toBeInTheDocument();
     expect(children).toHaveAttribute('href', '/my-link');
