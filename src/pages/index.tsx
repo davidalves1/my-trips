4118ec4ec6a58b8359f4f9d4c6dbfb78bd121137
davidalves1 <alves.david@outlook.com>
2021-04-21 15:09:50 -0300

    feat: create home template and add places query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import dynamic from 'next/dynamic';
-import { Info } from '@styled-icons/evaicons-solid/Info';
-import LinkWrapper from '@components/LinkWrapper';
+import client from '@graphql/client';
+import { GET_PLACES } from '@graphql/queries';
+import { MapProps } from '@components/Map';
+import HomeTemplate from '@templates/Home';
 
-const Map = dynamic(() => import('@components/Map'), { ssr: false });
-
-export default function Home() {
-  return (
-    <>
-      <LinkWrapper href="/about">
-        <Info size={32} />
-      </LinkWrapper>
-      <Map />
-    </>
-  );
+export default function Home({ places }: MapProps) {
+  return <HomeTemplate places={places} />;
 }
+
+export const getStaticProps = async () => {
+  const { places } = await client.request(GET_PLACES);
+
+  return { props: { places } };
+};
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,18 @@
+import dynamic from 'next/dynamic';
+
+import { MapProps } from '@components/Map';
+import { Info } from '@styled-icons/evaicons-solid/Info';
+import LinkWrapper from '@components/LinkWrapper';
+
+const Map = dynamic(() => import('@components/Map'), { ssr: false });
+
+export default function HomeTemplate({ places }: MapProps) {
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <Info size={32} />
+      </LinkWrapper>
+      <Map places={places} />
+    </>
+  );
+}
