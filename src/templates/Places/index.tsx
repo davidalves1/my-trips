588da27f143182895671f6fcd7925d29ae65e932
davidalves1 <alves.david@outlook.com>
2021-04-21 16:19:57 -0300

    feat: add style to place template

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,8 @@
+import LinkWrapper from '@components/LinkWrapper';
+import { Close } from '@styled-icons/evaicons-solid/Close';
+
+import * as S from './style';
+
 type ImageProps = {
   url: string;
   width: number;
@@ -18,13 +23,23 @@ export type PlacesTemplateProps = {
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
   return (
     <>
-      <h1>{place.name}</h1>
+      <LinkWrapper href="/">
+        <Close size={32} />
+      </LinkWrapper>
 
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
 
-      {place.gallery.map((photo, index) => (
-        <img key={`photo-${index}`} src={photo.url} alt={place.name} />
-      ))}
+          <S.Body dangerouslySetInnerHTML={{ __html: place.description.html }} />
+
+          <S.Gallery>
+            {place.gallery.map((photo, index) => (
+              <img key={`photo-${index}`} src={photo.url} alt={place.name} />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   );
 }
